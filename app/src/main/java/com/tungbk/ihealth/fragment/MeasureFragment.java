c7a12dfa061e45006bafe2e30a82670148a0ae6e
Nguyen Xuan Tung <nxtungbkhn@gmail.com>
2016-10-09 14:31:14 +0700

    update measure fragment

--- app/src/main/java/com/tungbk/ihealth/fragment/MeasureFragment.java
+++ app/src/main/java/com/tungbk/ihealth/fragment/MeasureFragment.java
@@ -1,17 +1,20 @@
 package com.tungbk.ihealth.fragment;
 
-import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.tungbk.ihealth.R;
+
+import java.util.Random;
 
 /**
  * Created by nxtbk on 10/9/2016.
@@ -20,6 +23,7 @@ import com.tungbk.ihealth.R;
 public class MeasureFragment extends Fragment {
 
     private ImageView heartRateButton;
+    private TextView tipTextView;
 
     @Nullable
     @Override
@@ -28,8 +32,6 @@ public class MeasureFragment extends Fragment {
 
         final int[] imgArr = {R.drawable.heart_rate, R.drawable.heart_rate_white};
 
-
-        heartRateButton = (ImageView) v.findViewById(R.id.heart_view_measure_image);
         final Handler handler = new Handler();
         Runnable runnable = new Runnable() {
             int i = 0;
@@ -44,6 +46,16 @@ public class MeasureFragment extends Fragment {
             }
         };
         handler.postDelayed(runnable, 300);
+
+        String[] tipArray = getResources().getStringArray(R.array.health_tips);
+
+        heartRateButton = (ImageView) v.findViewById(R.id.heart_view_measure_image);
+        tipTextView = (TextView) v.findViewById(R.id.tip_text_view);
+        tipTextView.setEllipsize(TextUtils.TruncateAt.MARQUEE);
+        tipTextView.setSelected(true);
+        tipTextView.setSingleLine(true);
+        Random r = new Random();
+        tipTextView.setText(tipArray[r.nextInt(tipArray.length)]);
 
         return v;
     }
